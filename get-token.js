6764a149d7b4b629afbc48f6fe4d20ee13a7d563
AyseTeam <86293015+AyseTeam@users.noreply.github.com>
2025-12-23 00:17:42 +0100

    Refactor token extraction in get-token.js
    Refactor Discord token extraction logic and cleanup.

--- get-token.js
+++ get-token.js
@@ -1,16 +1,26 @@
-var req = webpackJsonp.push([
-    [], {
-        extra_id: (e, t, r) => e.exports = r
-    },
-    [
-        ["extra_id"]
-    ]
+/**
+ * Discord Token Extractor
+ * Coded by @AyseTeam
+ */
+
+window.webpackChunkdiscord_app.push([
+    [Symbol()],
+    {},
+    o => {
+        for (let e of Object.values(o.c)) {
+            try {
+                if (!e.exports || e.exports === window) continue;
+                if (e.exports?.getToken) {
+                    token = e.exports.getToken();
+                }
+                for (let o in e.exports) {
+                    if (e.exports?.[o]?.getToken && "IntlMessagesProxy" !== e.exports[o][Symbol.toStringTag]) {
+                        token = e.exports[o].getToken();
+                    }
+                }
+            } catch (err) {}
+        }
+    }
 ]);
-for (let e in req.c)
-    if (req.c.hasOwnProperty(e)) {
-        let t = req.c[e].exports;
-        if (t && t.__esModule && t.default)
-            for (let e in t.default) "getToken" === e && (token = t.default.getToken())
-    } 
-    
-token
+window.webpackChunkdiscord_app.pop();
+console.log("%c Your Token: ", "background: #7289da; color: white; font-weight: bold;", token);
